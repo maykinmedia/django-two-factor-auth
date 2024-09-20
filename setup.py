451483841f52aaa104570ce5ca87d7664ab00350
Joeri Bekker <joeri@maykinmedia.nl>
2024-09-20 18:56:12 +0200

    :bug: Fixed incorrect dependancy format causing pip>=24 to fail.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(exclude=('example', 'tests')),
     install_requires=[
         'Django>=2.2.0,<4.0.0',
-        'django_otp>=1.0.0.<2.0.0',
+        'django_otp>=1.0.0,<2.0.0',
         'qrcode>=4.0.0,<6.99',
         'django-phonenumber-field>=5.0.0,<6.0.0',
         'django-formtools',
